56cc9080f4b2ebbab5f04a78204f9aa65e82db17
anukriti-ranjan <89630232+anukriti-ranjan@users.noreply.github.com>
2022-09-12 21:54:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import streamlit as st
+import time
 from haystack.utils import clean_wiki_text, convert_files_to_docs, fetch_archive_from_http, print_answers
 from haystack.nodes import FARMReader, TransformersReader
 from haystack.utils import launch_es
 
 
 launch_es()
+time.sleep(30)
 
 from haystack.document_stores import ElasticsearchDocumentStore
 from haystack.nodes import BM25Retriever
 
-document_store = ElasticsearchDocumentStore(host="localhost", username="", password="", index="document")
-#return_embedding=True
+#ConnectionError: Initial connection to Elasticsearch failed. Make sure you run an Elasticsearch instance at `[{'host': 'localhost', 'port': 9200}]` and that it has finished the initial ramp up (can take > 30s).
+#document_store = ElasticsearchDocumentStore(host="localhost", username="", password="", index="document")
+document_store = ElasticsearchDocumentStore(return_embedding=True)
+
 
 
 st.write("it's working!")
